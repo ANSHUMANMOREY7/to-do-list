edbd019a94368ab7de6dee535d110cb6829f5eab
ANSHUMANMOREY7 <anshumanmorey2@gmail.com>
2025-04-20 16:42:22 +0530

    object named tasklist created in js file

--- to-do-list.js
+++ to-do-list.js
@@ -4,5 +4,9 @@ function addtask() {
  const elementInput =document.getElementById("input-box")
  const elementpriority=document.getElementById("priority")
 
+ let tasklist ={
+  task : elementInput.value ,
+  priority:elementpriority.value || "low" 
+ };
 
 }
